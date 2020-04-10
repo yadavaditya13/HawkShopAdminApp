0015b9471479c25b5f095a8a9b67f2b4f496b3b3
yadavaditya13 <48686129+yadavaditya13@users.noreply.github.com>
2020-04-10 22:11:35 +0530

    commit 2

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:hawkshopadmin/providers/app_states.dart';
+import 'package:hawkshopadmin/providers/product_providers.dart';
+import 'package:hawkshopadmin/screen/dashboard.dart';
+import 'package:provider/provider.dart';
+
+void main(){
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider.value(value: AppState()),
+      ChangeNotifierProvider.value(value: ProductProvider()),
+    ],
+    child: MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Dashboard(),
+    ),
+  ));
+}
