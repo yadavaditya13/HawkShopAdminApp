5b3d9e0bcf0ebc053217ca0687953325daebcb01
yadavaditya13 <48686129+yadavaditya13@users.noreply.github.com>
2020-04-13 00:07:45 +0530

    updated

--- lib/providers/product_providers.dart
+++ lib/providers/product_providers.dart
@@ -1,3 +1,15 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:53ed49808d9c13db27598017452652fcfcf58289b9b3ebda89431252e1203312
-size 312
+import 'package:flutter/material.dart';
+
+class ProductProvider with ChangeNotifier{
+  List<String> selectedColors = [];
+
+  addColors(String color){
+    selectedColors.add(color);
+    notifyListeners();
+  }
+
+  removeColor(String color){
+    selectedColors.remove(color);
+    notifyListeners();
+  }
+}
